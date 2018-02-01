e104b8091834bc25674ffeb8a578674c5177a58c
MarekVrofski <markkusters92@gmail.com>
2018-02-01 21:47:23 +0100

    Added styling to the tables & extra fieldset for the autoloadDuration

--- admin/partials/uitdb-plugin-options-page-display.php
+++ admin/partials/uitdb-plugin-options-page-display.php
@@ -22,22 +22,26 @@ $aLoad = $ec->showAutoload();
 <div class="wrap">
     <form action="<?php __FILE__ ?>" method="post">
         <input type="hidden" name="type" value="keySecret">
-        <table>
+        <table class="table table-striped table-wrap">
+            <thead>
             <tr>
-                <th>Key & Secret</th>
-                <th></th>
+                <th colspan="4">Key & Secret</th>
             </tr>
+            </thead>
             <tr>
                 <td><label for="key">Key:</label></td>
-                <td><input type="text" name="key" placeholder="<?php echo isset($ksCombo) ? $ksCombo['uitdb_key'] : 'Key'; ?>"/></td>
+                <td><input class="table-wrap-ks" type="text" name="key" placeholder="<?php echo isset($ksCombo) ? $ksCombo['uitdb_key'] : 'Key'; ?>" value="<?php echo isset($ksCombo) ? $ksCombo['uitdb_key'] : 'Key'; ?>"/></td>
+                <td></td>
             </tr>
             <tr>
                 <td><label for="secret">Secret:</label></td>
-                <td><input type="text" name="secret" placeholder="<?php echo isset($ksCombo) ? $ksCombo['uitdb_secret'] : 'Secret'; ?>"/></td>
+                <td><input class="table-wrap-ks" type="text" name="secret" placeholder="<?php echo isset($ksCombo) ? $ksCombo['uitdb_secret'] : 'Secret'; ?>" value="<?php echo isset($ksCombo) ? $ksCombo['uitdb_secret'] : 'Secret'; ?>"/></td>
+                <td></td>
             </tr>
             <tr>
                 <td></td>
                 <td><button type="submit">Key & Secret gebruiken</button></td>
+                <td><button type="submit">Key & Secret updaten</button></td>
             </tr>
         </table>
     </form>
@@ -46,22 +50,35 @@ $aLoad = $ec->showAutoload();
 
     <form action="<?php __FILE__ ?>" method="post">
         <input type="hidden" name="type" value="loadEventsAuto">
-        <table>
+        <table class="table table-striped table-wrap">
+            <thead>
             <tr>
-                <th>Extra options</th>
-                <th></th>
+                <th colspan="2">Extra options</th>
             </tr>
+            </thead>
             <tr>
                 <td><label for="autoload">Autoload (daily):</label></td>
                 <td></td>
             </tr>
             <tr>
                 <fieldset id="autoload">
-                    <td>
+                    <td colspan="2">
                         <label>Yes:</label>
                         <input type="radio" value="Yes" name="autoloadYes">
                         <label>No:</label>
                         <input type="radio" value="No" name="autoloadNo">
+                    </td>
+                </fieldset>
+            </tr>
+            <tr>
+                <fieldset id="autoloadDuration">
+                    <td colspan="2">
+                        <label>Hourly:</label>
+                        <input type="radio" value="hourly" name="hourly">
+                        <label>Twicedaily:</label>
+                        <input type="radio" value="twicedaily" name="twicedaily">
+                        <label>Daily:</label>
+                        <input type="radio" value="daily" name="daily">
                     </td>
                 </fieldset>
             </tr>
